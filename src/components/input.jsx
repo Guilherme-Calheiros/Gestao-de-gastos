6b4ad2f7f54ab8657589d5d025fc4949edc30dc2
Guilherme-Calheiros <guilhermecalheiros522@gmail.com>
2024-07-02 17:00:30 -0300

    feat: adding UseState

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,7 +1,7 @@
 
-function Input({value , type, placeholder , onChange}) {
+function Input({value , type, placeholder , onChange, pattern, required, onInvalid}) {
     return(
-        <input className="p-2 placeholder:text-textcolor border border-textcolor rounded-sm w-64 md:w-80" value={value} type={type} placeholder={placeholder} onChange={onChange}/>
+        <input className="p-2 placeholder:text-textcolor border border-textcolor rounded-sm w-64 md:w-80" value={value} type={type} placeholder={placeholder} onChange={onChange} pattern={pattern} required={required} onInvalid={onInvalid}/>
     )
 }
 
--- src/login.jsx
+++ src/login.jsx
@@ -1,30 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./assets/Logo.svg"
 import ImagemGarota from "./assets/ImagemGarota.svg"
 import ImagemGaroto from "./assets/ImagemGaroto.svg"
 import Input from "./components/input.jsx";
+import { Link } from "react-router-dom";
 
 let imagens = [ImagemGarota, ImagemGaroto]
 const imageRandom = imagens[Math.floor(Math.random() * imagens.length)]
 
 function Login() {
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log(email, senha)
+  }
+
   return (
     <div className="flex h-full">
       <div className="flex flex-col justify-center items-center h-full w-full bg-gradient-to-b from-white to-primarycolor-100 md:w-1/2 md:bg-none">
-        <img src={Logo} alt="Logo FinFácil" className="w-64 mb-8"/>
-        <form className="flex flex-col gap-y-9 items-center border border-contrastcolor rounded-md p-2 bg-white md:w-96">
+        <img src={Logo} alt="Logo FinFácil" className="w-64 mb-8" />
+        <form className="flex flex-col gap-y-9 items-center border border-contrastcolor rounded-md p-2 bg-white md:w-96" onSubmit={handleSubmit}>
           <h1 className="text-3xl font-bold text-secondarycolor font-secondary">Login</h1>
-          <Input type="email" placeholder="Email" />
-          <Input type="password" placeholder="Senha" />
+          <Input type="email" placeholder="Email" onChange={e => setEmail(e.target.value)} required='required'/>
+          <Input type="password" placeholder="Senha" onChange={e => setSenha(e.target.value)} required='required' pattern='(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}'/>
           <div className="flex gap-4 md:gap-7">
-            <button className="bg-contrastcolor text-white p-3 w-32 rounded-xl">Entrar</button>
-            <button className="text-contrastcolor p-3 w-32 rounded-xl border border-contrastcolor">Registrar-se</button>
+            <button type="submit" className="bg-contrastcolor text-white p-3 w-32 rounded-xl">Entrar</button>
+            <Link to='/register'><button className="text-contrastcolor p-3 w-32 rounded-xl border border-contrastcolor">Registrar-se</button></Link>
           </div>
           <a href="*" className="mb-4 text-textcolor font-extralight">Esqueci minha senha</a>
         </form>
       </div>
       <div className="bg-primarycolor-100 w-1/2 h-full hidden items-center justify-center md:flex">
-        <img src={imageRandom} alt="" />
+        <img src={imageRandom} alt="Imagem ilustrativa com gráficos" />
       </div>
     </div>
   );
